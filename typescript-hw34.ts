34bdbf86f8781f997eaa24def7b23445d1712427
Volha Maksimava <dobredumaj@gmail.com>
2023-04-05 17:27:48 +0300

    feat(hw-34): start hw

---
+++ typescript-hw34.ts
@@ -0,0 +1,33 @@
+// 1. 
+type Faculties = {
+    id: number,
+    faculty: string,
+	subjects: string[],
+	countStudents: number,
+}
+const faculties = [
+    {
+		id: 1,
+		faculty: "History department",
+		subjects: ["The World History", "History of Rome"],
+		countStudents: 44
+	}, 
+	{
+		id: 2,
+		faculty: "Department of Biology",
+		subjects: ["biology", "chemistry"],
+		countStudents: 50
+	},
+	{
+		id: 3,
+		faculty: "Faculty of Mathematics",
+		subjects: ["mathematics", "geometry", "trigonometry"],
+		countStudents: 72
+	},
+	{
+		id: 4,
+		faculty: "Faculty of Design",
+		subjects: ["ui", "ux", "graphic design"],
+		countStudents: 37
+	}
+];
