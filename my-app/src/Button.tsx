b215eb59c524dacd19d71eea4650e97192d37e5c
Volha Maksimava <dobredumaj@gmail.com>
2023-04-16 15:23:25 +0300

    feat(hw-37): add tasks 1, 2

---
+++ my-app/src/Button.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+import styles from './button.module.css';
+
+type Props = {
+    visible: boolean;
+    handleClick?: () => void;
+}
+export const Button = ({ visible, handleClick}: Props) => (
+    <button className={styles.button} disabled={!visible} onClick={handleClick}>
+        <span className={styles.burger_line_first}></span>
+        <span className={styles.burger_line_second}></span>
+        <span className={styles.burger_line_third}></span>
+
+        <span className={styles.close_line_first}></span>
+        <span className={styles.close_line_second}></span>
+    </button>
+)
+
