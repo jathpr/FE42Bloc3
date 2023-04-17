b67e8150f199171fff747cbf8729e26e1ffdb48c
Dzmitry Martyniuk <jathpr@gmail.com>
2023-04-17 13:07:37 +0400

    add test ts

---
+++ test.js
@@ -0,0 +1,5 @@
+var obj = { id: "str" };
+console.log("🚀 ~ file: test.ts:3 ~ obj:", obj.id);
+var arr = [1, 2];
+var x = arr.filter(function (a) { return a === 2; });
+console.log("🚀 ~ file: test.ts:7 ~ x:", x);
---
+++ test.ts
@@ -0,0 +1,7 @@
+const obj: { id: string } = { id: "str" };
+
+console.log("🚀 ~ file: test.ts:3 ~ obj:", obj.id);
+
+const arr = [1, 2];
+const x = arr.filter((a) => a === 2);
+console.log("🚀 ~ file: test.ts:7 ~ x:", x);
