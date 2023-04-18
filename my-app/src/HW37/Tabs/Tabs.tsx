106af2b4c50ee2a073bc5c768686627184fb15e9
polishukova <pollimagenta@gmail.com>
2023-04-18 16:24:05 +0200

    fix(hw-37): fixed tabs

--- my-app/src/HW37/Tabs/Tabs.tsx
+++ my-app/src/HW37/Tabs/Tabs.tsx
@@ -1,14 +1,17 @@
-import styles from "./Tabs.module.css"
+import React from "react"
+import "./Tabs.css"
 
 export const tabNames = ['All', 'My Favorites', 'Popular']
 
 type Tabs = {
     tabs: string[],
     activeTabName: string,
+    active: boolean,
 }
 
 export const Tabs = ({tabs, activeTabName}: Tabs) => (
-    <ul className={styles.tabs}>
-        {tabs.map(tab=><li className={tab===activeTabName? (styles.tabs--active): classB} key={tab.id}>{tab}</li>)}
+    <ul className='tabs'>
+        {tabs.map(tab=><li key={tab} className={tab===activeTabName ? 'tabs--active': ''}>{tab}</li>)}
     </ul>
 )
+
