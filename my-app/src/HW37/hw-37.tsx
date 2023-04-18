52c4bf6a78beb6b6a364c108c26d1c242db5b61b
polishukova <pollimagenta@gmail.com>
2023-04-18 12:04:40 +0200

    feat(hw-37): fixed button

--- my-app/src/HW37/hw-37.tsx
+++ my-app/src/HW37/hw-37.tsx
@@ -1,28 +1,15 @@
-import { Button } from "./buttonBurger"
-import { Title } from "./title";
-import style from "./buttonBurger.module.css"
-import { Tabs } from "./tabs";
-// import styles from "./tabs.module.css"
+import { Button } from "./Button/ButtonBurger"
+import { Title } from "./Title/Title";
+import style from "./Button/ButtonBurger.module.css"
+import { Tabs, tabNames } from "./Tabs/Tabs";
+import margin from "./HW-37.module.css"
 
 export const Hw37 = () => (
-    <div className={style.div}>
+    <div className={margin.div}>
         <Title />
         <div className={style.buttonDiv}>
-            <Button touched handleClick={() => console.log('OK')}>
-                <div>
-                    <hr className={style.burgerSpan}></hr>
-                    <hr className={style.burgerSpan}></hr>
-                    <hr className={style.burgerSpan}></hr>
-                </div>
-            </Button>
-            <Button handleClick={() => console.log('OK')}>
-                <div>
-                    <span className={style.closeSpan}>✖</span>
-                </div>
-            </Button>
+            <Button touched handleClick={() => console.log('OK')}/>
         </div>
-        <div>
-            <Tabs tabs={['All', 'My Favorites', 'Popular']}></Tabs>
-        </div>
+        <Tabs tabs={tabNames}/>
     </div>
 )
