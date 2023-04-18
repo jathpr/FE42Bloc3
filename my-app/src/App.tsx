4404f0a8074e6719f3c9ab407fa4af5ee4263d24
Viktar Maksimau <victornahimov@gmail.com>
2023-04-18 23:48:46 +0300

    feat(hw_37)fix bugs

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Title } from "./Title";
-import { Burger } from "./BurgerMenu"
-import { Tabs } from "./Tabs"
+import { ButtonComponent } from "./HW_37/BurgerMenu"
+import { Tabs } from "./HW_37/Tabs"
+import { Auth } from "./Auth"
+
 
 export const App  = () => (
   <div>
   <Title />
-  <Burger enabled handleclick={()=> console.log('click')} />
-  <Tabs tabs  = {['All', 'My favorites', 'Popular']}
-  handleclick ={()=> console.log('click')} />
+  <ButtonComponent enabled  />
+  <Tabs tabs  = {['All', 'My favorites', 'Popular']} activeTab='All'/>
+  {/* <Auth></Auth> */}
   </div>
 )
 
