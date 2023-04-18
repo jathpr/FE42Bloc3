901ad94da1fa4081ba2602801eb0e64139964afe
Olga Maksimova <106390669+OlechkaM@users.noreply.github.com>
2023-04-18 20:18:53 +0300

    Olga hw 37 (#40)
    * feat(hw-33): start hw
    
    * feat(hw-33): complete hw
    
    * feat(hw-34): start hw
    
    * feat(hw-34): complete homework
    
    * feat(hw-33): start hw
    
    * feat(hw-33): complete hw
    
    * feat(hw-34): start hw
    
    * feat(hw-34): complete homework
    
    * init(react): create react app
    
    * feat(hw-37): add tasks 1, 2
    
    * feat(hw-37): finish hw-37
    
    * feat(hw-37): finish hw-37
    
    * easy way
    
    ---------
    
    Co-authored-by: Dmitry Martyniuk <jathpr@gmail.com>

---
+++ my-app/src/Button/ButtonClass.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styles from './button.module.css';
+
+type Props = {
+    visible: boolean;
+    // handleClick: () => void;
+}
+
+type State = {
+    opened: boolean
+}
+export class ButtonComponent extends React.Component<Props, State> {
+    state = {opened: !!this.props.visible}
+    
+    click = () => {
+        // this.props.handleClick()
+        this.setState({opened: !this.state.opened})
+    }
+
+    render () {
+        return (
+            <button className={styles.button} onClick={this.click}>{this.state.opened ? <Burger /> : <Close />}  </button>
+        )
+    }
+
+}
+function Burger() {
+    return (
+        <div>
+            <span className={styles.burger_line_first}></span>
+            <span className={styles.burger_line_second}></span>
+            <span className={styles.burger_line_third}></span>
+        </div>
+    );
+}
+
+const Close = () => (
+    <div>
+        <span className={styles.close_line_first}></span>
+            <span className={styles.close_line_second}></span>
+    </div>
+)
