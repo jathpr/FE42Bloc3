9ea389bf4758005191f3e0580dde919f4c7bf91f
Dmitry Martyniuk <jathpr@gmail.com>
2023-04-18 20:36:15 +0400

    Olechka m (#42)
    * docs(cw_2): typescript first class work
    
    * feat(hw-33): start hw
    
    * docs(hw_2): add first typescript hw
    
    * docs(cw_3): typescript advansed
    
    * feat(hw-33): complete hw
    
    * feat(hw-34): start hw
    
    * feat(hw-34): complete homework
    
    * docs(hw_3): typescript advansed
    
    * docs(hw_3): typescript advansed
    
    * feat(hw-35): add hw-35
    
    * docs(hw_cw_4): add FP tasks
    
    * docs(hw_4): add hw data
    
    * docs(hw_cw_5): react props
    
    * feat(hw-33): start hw
    
    * feat(hw-33): complete hw
    
    * feat(hw-34): start hw
    
    * feat(hw-34): complete homework
    
    * init(react): create react app
    
    * fix(hw-35): fix mistakes
    
    * docs(hw6): state
    
    * feat(hw-33): start hw
    
    * feat(hw-33): complete hw
    
    * feat(hw-34): start hw
    
    * feat(hw-34): complete homework
    
    * init(react): create react app
    
    ---------
    
    Co-authored-by: Volha Maksimava <dobredumaj@gmail.com>

---
+++ hw-33.js
@@ -0,0 +1,70 @@
+const subjects = {
+    mathematics: {
+        students: 200,
+        teachers: 6
+    },
+    biology: {
+        students: 120,
+        teachers: 6
+    },
+    geography: {
+        students: 60,
+        teachers: 2
+    },
+    chemistry: {
+        students: 100,
+        teachers: 3
+    }
+}
+
+// 1. Создать строку из названий предметов написаных через запятую
+function writeSubjects(object) {
+    let string = ''
+    for (const key in object) {
+        string = string + key + ', '
+    }
+    return string.slice(0, -2)
+}
+console.log(writeSubjects(subjects));
+
+// 2. Посчитать общее количeство студентов и учителей на всех предметах
+function countPeople(object) {
+    let people = 0
+    for (const key in object) {
+        for (const i in object[key]) {
+            people = people + object[key][i]
+        }
+    }
+    return people
+}
+console.log(countPeople(subjects));
+
+// 3. Получить среднее количество студентов на всех прeдметах
+function countAverageStudents(object) {
+    let allStudents = 0
+    let count = 0
+    for (const key in object) {
+        allStudents = allStudents + object[key]['students']
+        count += 1
+    }
+    return allStudents / count
+}
+console.log(countAverageStudents(subjects));
+
+// 4. Создать массив из объектов предметов
+function createSublectsArray(object) {
+    let subjectsArray = []
+    for (const key in object) {
+        subjectsArray.push(object[key])
+    }
+    return subjectsArray
+}
+console.log(createSublectsArray(subjects));
+
+// 5. Получить массив из предметов и отсортировать по количеству преподавателей на факультете от большего к меньшему
+function sortTeachers(object) {
+    const subjectsArray = createSublectsArray(object)
+    const sortedArray = subjectsArray.sort((a, b) => a.teachers < b.teachers ? 1 : -1)
+    return sortedArray
+}
+console.log(sortTeachers(subjects))
