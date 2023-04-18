6b6c825e33e0a4f8624ab5917e7abfdf9d2797c6
Olga Maksimova <106390669+OlechkaM@users.noreply.github.com>
2023-04-18 20:18:53 +0300

    Olga hw 37 (#40)
    * feat(hw-33): start hw
    
    * feat(hw-33): complete hw
    
    * feat(hw-34): start hw
    
    * feat(hw-34): complete homework
    
    * feat(hw-33): start hw
    
    * feat(hw-33): complete hw
    
    * feat(hw-34): start hw
    
    * feat(hw-34): complete homework
    
    * init(react): create react app
    
    * feat(hw-37): add tasks 1, 2
    
    * feat(hw-37): finish hw-37
    
    * feat(hw-37): finish hw-37
    
    * easy way
    
    ---------
    
    Co-authored-by: Dmitry Martyniuk <jathpr@gmail.com>

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Title } from './Title';
+import { Button } from './Button/Button';
+import { Tabs } from './Tabs/Tabs';
+import { ButtonComponent } from './Button/ButtonClass';
+
+
 
 function App() {
+  const tabNames = ['All', 'My favourites', 'Popular']
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title />
+      <Button visible handleClick={() => console.log('ok')} />
+      <Tabs tabs={tabNames} activeTab='My favourites' />
+
+      <ButtonComponent visible />
     </div>
   );
 }
