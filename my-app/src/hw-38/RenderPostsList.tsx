53c58b9911b8a0324e62206a9bc34a87fa576d0c
Olga Maksimova <106390669+OlechkaM@users.noreply.github.com>
2023-04-26 15:42:42 +0300

    Olga hw 38 (#52)

---
+++ my-app/src/hw-38/RenderPostsList.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Post, postsList } from './posts'
+import { RenderPost } from './RenderPost'
+
+
+
+// export class RenderPostsList extends React.Component {
+//     state:{post: Post[]} = {post: postsList}
+
+//     render() {
+//         return this.state.post.map(post => <RenderPost post1={post} key={post.id} />)
+//     }
+// }
+
+
+export const RenderPostsList = (props: { array: Post[] }) => {
+    return <> 
+    {props.array.map(post => <RenderPost post1={post} key={post.id}/>)  }
+    
+    </>
+}
+
