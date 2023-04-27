0829e8f2030e79d85d71f6b5a27b5aaa4c665967
Volha Maksimava <dobredumaj@gmail.com>
2023-04-27 19:27:45 +0300

    feat(hw-40): add styles + add Registration

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Title } from './Title';
 // import { Button } from './Button/Button';
-import { Tabs } from './Tabs/Tabs';
 import { Button } from './Button/ButtonClass';
+import { Tabs } from './Tabs/Tabs';
 import { RenderPostsList } from './hw-38/RenderPostsList';
-import { postsList } from './hw-38/posts';
+import { Registration } from './Registration';
+import { Auth } from './Auth';
 
+type User = {
+  login: string,
+  password: string
+}
 
+type Pages = 'auth' | 'reg'
 
-function App() {
-  const tabNames = ['All', 'My favourites', 'Popular']
+const tabNames = ['All', 'My favourites', 'Popular']
+
+export const App = () => {
+  const [users, setUsers] = useState<User[]>([])
+  const [page, setPage] = useState<Pages>('reg')
+
+  const addUser = (login: string, password: string) => {
+    setUsers([...users, { login, password }])
+  }
+
+  const checkUser = (login: string, password: string) => {
+    const result = users.find(user => (user.login === login) && (user.password === password))
+    console.log("🚀 ~ file: App.tsx:28 ~ checkUser ~ result:", result)
+  }
+
+useEffect(()=>{setPage('auth')}, [users])
+
+const navToReg = () => {
+  setPage('reg')
+}
+
   return (
     <div>
-      <Title />
       {/* <Button visible handleClick={() => console.log('ok')} /> */}
-      <Tabs tabs={tabNames} activeTab='My favourites' />
+      <Title />
       <Button visible handleClick={() => console.log('ok')} />
-      <RenderPostsList array={postsList}/>
+      <Tabs tabs={tabNames} activeTab='My favourites' />
+      <RenderPostsList />
+      {page === 'reg' && <Registration onReg={addUser} />}
+      {page === 'auth' && <Auth onAuth={checkUser} onSignUp={navToReg} />}
     </div>
   );
 }
 
-export default App;
