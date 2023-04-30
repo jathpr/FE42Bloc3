707c124056c29235e10a09780092a50f6dd961a7
Hannah <118190915+hannadzenis@users.noreply.github.com>
2023-04-30 22:11:35 +0200

    feat: solutions for hw37

---
+++ Hanna/app-hw/my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import './index.css';
+import { Title } from './hw37/title/title';
+import { Menu } from './hw37/menu-burger/menu';
+import  { Tabs } from './hw37/tabs/tabs';
+
+function App() {
+return (
+    <>
+		<h1>HW37</h1>
+		<Title></Title>
+		<Menu></Menu>
+		<Tabs></Tabs>
+	</>
+);
+}
+
+export default App;
---
+++ Hanna/app-hw/my-app/src/hw37/title/title.tsx
@@ -0,0 +1,4 @@
+import React, { ReactNode } from 'react';
+import './title.css';
+
+export const Title = () => (<h1 className='title'>Sign In</h1>)
