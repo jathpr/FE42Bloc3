ae912002dee78fbdbbddb3b7a029dc1423b53c91
Volha Maksimava <dobredumaj@gmail.com>
2023-05-01 16:46:03 +0300

    change styles, delete foders

--- my-app/src/Registration.tsx
+++ my-app/src/Registration.tsx
@@ -1,18 +1,23 @@
 import { useState } from "react"
+import style from './registration.module.css';
 
 type Props = {
     onReg: (login: string, password: string) => void
 }
 
-export const Registration = ({onReg}: Props) => {
+export const Registration = ({ onReg }: Props) => {
 
     const [login, setLogin] = useState('')
     const [password, setPassword] = useState('')
 
     return <>
-    <input value={login} onChange={(e) => { setLogin(e.currentTarget.value)}} />
-    <input value={password} onChange={(e) => { setPassword(e.currentTarget.value)}} />
-    <button onClick={() => onReg(login, password)}>Registration</button>
+        <div className={style.registr_wrapper}>
+            <span className={style.registr_login}>Login</span>
+            <input className={style.registr_input} value={login} onChange={(e) => { setLogin(e.currentTarget.value) }} />
+            <span className={style.registr_password}>Password</span>
+            <input className={style.registr_input} value={password} onChange={(e) => { setPassword(e.currentTarget.value) }} />
+            <button className={style.registr_button} onClick={() => onReg(login, password)}>Registration</button>
+        </div>
     </>
 
 }
