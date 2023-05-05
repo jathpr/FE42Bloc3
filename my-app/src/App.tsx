de735cb312450f7311ea0063c5f6c27c094d009a
Dmitry Martyniuk <jathpr@gmail.com>
2023-05-05 20:57:58 +0400

    Revert "HW 41 Polina (#63)"
    This reverts commit 0ad9b5b6abcc464bf0d8b3064f0e5f6781af26dc.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './App.css'
-import { Button } from './Components/BurgerMenu/ButtonBurger';
+import { Button } from './Components/Button/ButtonBurger';
 import { Registration } from './Components/Registration/Registration';
 import { Authorization } from './Components/Authorization/Authorization';
 import { LanguageContext } from './Lessons/languageContext';
 import { PostsList } from './Components/Post/PostsList';
-import { Search } from './Components/Search/Search';
 
 
 
@@ -16,23 +15,21 @@ type User = {
 
 type Pages = 'Auth' | 'Reg' | 'Posts'
 
-type SearchResult = { result: string }
-
 export const App = () => {
   const [users, setUsers] = useState<User[]>([])
   const [page, setPage] = useState<Pages>('Reg')
-  const [searchResult, setSearchResult] = useState('')
 
   const addUser = (login: string, password: string) => {
     setUsers([...users, { login, password }])
+    // setPage('Auth')
   }
 
   const checkUser = (login: string, password: string) => {
     const findUser = users.find(user => user.login === login && user.password === password)
-    // if (findUser) {
+    if (findUser){
       setPage('Posts')
-    // }
-    // else alert('Wrong data')
+    }
+    else alert('Wrong data')
   }
 
   const navToReg = () => {
@@ -41,20 +38,26 @@ export const App = () => {
 
   useEffect(() => { setPage('Auth') }, [users])
 
-  const changeSearchResult = (result: string) => {
-    setSearchResult(result)
-  }
-
-  // useEffect(() => {setSearchResult('')})
+  function TextInputWithFocusButton() {
+  const inputEl = useRef<HTMLInputElement>(null);
+  const onButtonClick = () => {
+    // `current` указывает на смонтированный элемент `input`
+    inputEl.current?.focus();
+  };
+  return (
+    <>
+      <input ref={inputEl} type="text" />
+      <button onClick={onButtonClick}>Установить фокус на поле ввода</button>
+    </>
+  );
+}
 
   return (
     <>
+    {TextInputWithFocusButton()}
       <header className='header'>
         <div>
           <Button touched handleClick={() => console.log('OK')} />
-        </div>
-        <div>
-          <Search onSearch={changeSearchResult} />
         </div>
       </header>
       <body>
@@ -64,9 +67,7 @@ export const App = () => {
             {page === 'Auth' && <Authorization onAuth={checkUser} onSignUp={navToReg} />}
           </div>
           <div>
-            {page === 'Posts' && <><p>Search results: {searchResult}</p>
-            <PostsList searchResult={searchResult} /></>
-            }
+            {page === 'Posts' && <PostsList />}
           </div>
         </div>
       </body>
--- my-app/src/Components/Post/PostsList.tsx
+++ my-app/src/Components/Post/PostsList.tsx
@@ -2,14 +2,13 @@ import { useEffect, useState } from "react"
 import { RenderPost } from "./RenderPost";
 import { Post, getPosts } from "../getPosts";
 
-type Props = { searchResult?: string }
 
-export const PostsList = ({ searchResult }: Props) => {
+export const PostsList = () => {
     const [cards, setCards] = useState<Post[]>([])
 
-    useEffect(() => { getPosts({ 'limit': 4, 'search': searchResult }).then((resp) => setCards(resp)) }, [])
+    useEffect(() => {getPosts({'limit': 4, 'search': 'ast'}).then((resp) => setCards(resp))}, [])
 
     return <>
-        {cards.map(post => <RenderPost post={post} key={post.id} />)}
+    {cards.map(post => <RenderPost post={post} key={post.id} />)}
     </>
 }
