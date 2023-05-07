834a42887517b2537b5cc1f9f348c1f66bf1bc7c
polishukova <pollimagenta@gmail.com>
2023-05-07 14:49:36 +0200

    fix(hw-41): fix search and add links

--- my-app/src/Components/Post/PostsList.tsx
+++ my-app/src/Components/Post/PostsList.tsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from "react"
 import { RenderPost } from "./RenderPost";
-import { Post, getPosts } from "../getPosts";
+import { OnePost, getPosts } from "./getPosts";
+import { useNavigate } from "react-router-dom";
 
-type Props = { searchResult?: string }
+type Props = { searchResult: string }
 
 export const PostsList = ({ searchResult }: Props) => {
-    const [cards, setCards] = useState<Post[]>([])
+    const [cards, setCards] = useState<OnePost[]>([])
 
-    useEffect(() => { getPosts({ 'limit': 4, 'search': searchResult }).then((resp) => setCards(resp)) }, [])
+    useEffect(() => {
+        getPosts({ 'limit': 4, 'search': searchResult }).then((resp) => setCards(resp))
+    }, [searchResult])
+
+    const navigate = useNavigate()
 
     return <>
-        {cards.map(post => <RenderPost post={post} key={post.id} />)}
+        {cards.map(post => <li onClick={() => navigate('/Posts/' + post.id)}><RenderPost post={post} key={post.id} /></li>)}
     </>
 }
