d01041eec36600b117fe24609466826acad2a6f2
Mary <mary.sk2000@gmail.com>
2023-06-29 20:21:47 +0300

    init(hw_11): finish hw-11

--- my-app/src/Components/PostsList.tsx
+++ my-app/src/Components/PostsList.tsx
@@ -4,15 +4,22 @@ import { Post } from "./Post";
 import { OnePost, getPosts } from "../server/getPosts";
 import { searchPosts } from "../server/searchPosts";
 import { Link } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../store/store";
+import { setPosts } from "../store/post";
+import { PopupImg } from "./PopupImg";
 
 type Props = {
    searchInputValue?: string,
-   onPostClick: (post: OnePost, e: React.MouseEvent<HTMLLIElement, MouseEvent>) => void
+   // onPostClick: (post: OnePost, e: React.MouseEvent<HTMLLIElement, MouseEvent>) => void
 }
 
-export const PostsList = ({ searchInputValue, onPostClick }: Props) => {
+export const PostsList = ({ searchInputValue }: Props) => {
 
-   const [posts, setPosts] = useState<OnePost[]>([]);
+   // const [posts, setPosts] = useState<OnePost[]>([]);
+
+   const posts = useAppSelector(state => state.posts.posts)
+   const postImg = useAppSelector(state => state.posts.postImg)
+   const dispatch = useAppDispatch()
 
    // const [searchValue, setSearchValue] = useState(searchInputValue)
 
@@ -22,15 +29,17 @@ export const PostsList = ({ searchInputValue, onPostClick }: Props) => {
 
       if (searchInputValue) {
          searchPosts({ search: searchInputValue })
-            .then(serverPosts => setPosts(serverPosts))
+            // .then(serverPosts => setPosts(serverPosts))
+            .then(serverPosts => dispatch(setPosts(serverPosts)))
       }
       else {
          getPosts({ limit: 8 })
             .then((serverPosts: OnePost[]) => {
-               setPosts(serverPosts)
+               // setPosts(serverPosts)
+               dispatch(setPosts(serverPosts))
             })
       }
-   }, [posts])
+   }, [searchInputValue])
 
    // useEffect(() => {
    //    console.log('effect-1');
@@ -53,10 +62,21 @@ export const PostsList = ({ searchInputValue, onPostClick }: Props) => {
 
    // }, [searchValue])
 
+
+
+
+   const showImgPopup = () => {
+      console.log('state in list', postImg.id)
+      console.log('state in list', postImg.image)
+   }
+
    return <>
+      {/* <p>{postImg.id}</p> */}
       <ul className={styles['posts-list']}>
-         {posts.map((post: OnePost) => <Link className={styles['one-post']} to={'/posts/' + post.id}><li key={post.id}><Post post={post} /></li></Link>)}
+         {posts.map((post: OnePost) => <li className={styles['one-post']} key={post.id}><Post post={post} onImgClick={showImgPopup} /></li>)}
+         {/* {posts.map((post: OnePost) => <Link className={styles['one-post']} to={'/posts/' + post.id}><li key={post.id}><Post post={post} /></li></Link>)} */}
          {/* {posts.map((post: OnePost) => <li onClick={(e) => onPostClick(post, e)} key={post.id} className={styles['one-post']}><Post post={post} /></li>)} */}
       </ul>
+      <PopupImg />
    </>
 }
